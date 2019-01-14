7729efd9a03d3740d9e4d2bdb70185247fd36a90
Rodolfo Silva <rodolfoslv@gmail.com>
2019-01-14 16:35:39 -0200

    update

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
-const argv = yargs.argv;
+const titleObtion = { describe: 'Title of note', demand: true, alias: 't' }
+const bodyOption = { describe: 'The body of the note', demand: false, alias: 'b'}
+const argv = yargs
+.command('add','Add a new note', {
+	title: titleObtion,
+	body: bodyOption
+})
+.command('list', 'List all notes')
+.command('read', 'Read a note', {
+	title: titleObtion,
+	body: bodyOption
+})
+.command('remove', 'Remove a note', {
+	title: titleObtion,
+	body: bodyOption
+})
+.help()
+.argv;
 
 let command = argv._[0];
 
